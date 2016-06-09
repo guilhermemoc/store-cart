464c807ec6eda900a4f26864724ad4686c4d191b
Guilherme Martins <adm.gms@gmail.com>
2016-06-09 16:33:39 -0300

    fcm

---
+++ js/function.js
@@ -0,0 +1,12 @@
+  //
+  //   $('label').click(function() {
+  //     $('body').css("background-color","rgba(0,0,0,0.7)");
+  //   });
+  //   $('label').click(function() {
+  //     $('body').css("background-color","black");
+  //   });
+  //
+  //   $('.close').on('click', function(e) {
+  //   $('.overlay').toggleClass; //you can list several class names
+  //   e.preventDefault();
+  // });
